8dbf692705b834ad5a661585270b2164b23763df
vert3xo <skvertexo@gmail.com>
2022-01-10 11:17:59 +0100

    remove constants

--- server/pkg/data/constants.go
+++
@@ -1,7 +0,0 @@
-package constants
-
-var SIGNING_KEY = []byte("secret")
-var SERVICES_LOCATION = "/home/tor/hidden_services"
-var WEB_LOCATION = "/var/www"
-var ADDRESS_GENERATOR = "/home/vert3xo/Documents/school/hosting/onion_utils/address_generator"
-var TOR_USER = "tor"
